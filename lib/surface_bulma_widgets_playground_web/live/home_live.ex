16c1e94290416bd30869eb485b28041756b76501
Jaremy Creechley <creechley@gmail.com>
2021-05-22 02:59:34 -0700

    basic setup

--- lib/surface_bulma_widgets_playground_web/live/home_live.ex
+++ lib/surface_bulma_widgets_playground_web/live/home_live.ex
@@ -1,10 +1,17 @@
 defmodule SurfaceBulmaWidgetsPlaygroundWeb.HomePageLive do
   use Surface.LiveView
 
+  require Logger
+
   alias Surface.Components.LiveRedirect
 
+  use SurfaceBulmaWidgets
   alias SurfaceBulmaWidgets.Components.CardWithIcon
   alias SurfaceBulmaWidgets.Layouts.{Columns, Column}
+
+  alias SurfaceBulmaWidgets.UI.RangedSlider
+
+  data count, :integer, default: 10
 
   def render(assigns) do
     ~H"""
@@ -48,10 +55,19 @@ defmodule SurfaceBulmaWidgetsPlaygroundWeb.HomePageLive do
               and more. You can also use them as templates to
               create your own custom components.
             </p>
+
+            <RangedSlider id="rl1" var={{bind(@count)}}>
+            </RangedSlider>
+
             <p><LiveRedirect label="Learn more" to="/uicomponents" /></p>
           </CardWithIcon>
         </Column>
       </Columns>
     """
+  end
+
+  def handle_info({:update_bind, name, value} = msg, socket) do
+    Logger.info("updating: #{inspect msg}")
+    {:noreply, socket |> assign([{name, value}]) }
   end
 end
