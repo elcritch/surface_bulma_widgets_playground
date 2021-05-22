3de8b81c55c2d2044fe29349d7799952130841d8
Jaremy Creechley <creechley@gmail.com>
2021-05-22 01:09:34 -0700

    adding bulma css'es

--- mix.exs
+++ mix.exs
@@ -33,6 +33,9 @@ defmodule SurfaceBulmaWidgetsPlayground.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:surface, "~> 0.4.0"},
+      {:surface_bulma_widgets, "~> 0.1.0", path: "../surface_bulma_widgets"},
+
       {:phoenix, "~> 1.5.8"},
       {:phoenix_live_view, "~> 0.15.1"},
       {:floki, ">= 0.27.0", only: :test},
